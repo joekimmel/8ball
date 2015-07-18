2257c6ad71c55d80eeea53ead24b7ed7f4623296
Joe Kimmel <joe.kimmel@appfolio.com>
2015-07-18 07:21:18 -0700

    shaking should reload the page

--- app/assets/javascripts/custom_shake_handler.js
+++ app/assets/javascripts/custom_shake_handler.js
@@ -3,6 +3,6 @@ window.onload = function() {
     var myShakeEvent = new Shake({ threshold: 15, timeout: 1000 });
     myShakeEvent.start();
     window.addEventListener('shake', shakeEventDidOccur, false);
-    function shakeEventDidOccur() { alert('shake!'); }
+    function shakeEventDidOccur() { window.location.reload(true); }
 };
 
