912a354534da091c237e130f1db99dcd5b7b71c8
Joe Kimmel <joe.kimmel@appfolio.com>
2015-07-17 22:27:56 -0700

    might be enabling shake.js

---
+++ app/assets/javascripts/custom_shake_handler.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+
+    var myShakeEvent = new Shake({ threshold: 15, timeout: 1000 });
+    myShakeEvent.start();
+    window.addEventListener('shake', shakeEventDidOccur, false);
+    function shakeEventDidOccur() { alert('shake!'); }
+};
+
