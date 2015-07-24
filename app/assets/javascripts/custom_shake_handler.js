89c0c6cdcca2a0dcc67d9e413678510ae41d5613
Joe Kimmel <joe.kimmel@appfolio.com>
2015-07-23 22:40:47 -0700

    javascript shake handler should submit form rather than reload page

--- app/assets/javascripts/custom_shake_handler.js
+++ app/assets/javascripts/custom_shake_handler.js
@@ -3,6 +3,8 @@ window.onload = function() {
     var myShakeEvent = new Shake({ threshold: 15, timeout: 1000 });
     myShakeEvent.start();
     window.addEventListener('shake', shakeEventDidOccur, false);
-    function shakeEventDidOccur() { window.location.reload(true); }
+    function shakeEventDidOccur() {
+      document.getElementById("shakeForm").submit();
+    }
 };
 
